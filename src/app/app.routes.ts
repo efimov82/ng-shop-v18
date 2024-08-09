18135af3cb0d8a040920d2d80b723dada53b42ef
Danil Efimov <danil.efimov@aventusgroup.com>
2024-08-09 16:26:06 +0300

    Migrate to v18

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { CheckoutComponent, HomeComponent, ThanksComponent } from './pages';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: ':page', component: HomeComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'thanks', component: ThanksComponent },
+  { path: ':page', component: HomeComponent },
 ];
