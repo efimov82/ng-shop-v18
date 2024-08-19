a3b988ea52498a0bf694a7ca8f03e67eec8707d9
Danil Efimov <danil.efimov@aventusgroup.com>
2024-08-19 04:59:16 +0300

    Add ignore auto generated models

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,7 +1,12 @@
 export * from './DeliveryType';
 export * from './IAddress';
 export * from './ICartItem';
-export * from './IOrder';
+// export * from './IOrder';
 export * from './IOrderState';
 export * from './IPersonalData';
-export * from './IProduct';
+// export * from './IProduct';
+
+// Generated modeles
+export * from './generated/model/order';
+export * from './generated/model/orderItem';
+export * from './generated/model/product';
