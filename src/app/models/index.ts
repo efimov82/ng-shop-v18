e79d6a6078acb017d126989d0a969dbfeac126da
Danil Efimov <danil.efimov@aventusgroup.com>
2024-08-25 04:08:01 +0300

    feat-1: fix build

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -9,7 +9,7 @@ export * from './IPersonalData';
 // Generated modeles
 export * from './generated/model/createOrderRequest';
 export * from './generated/model/createUserDto';
-export * from './generated/model/order';
+export * from './generated/model/customerOrder';
 export * from './generated/model/orderItem';
 export * from './generated/model/orderItemDto';
 export * from './generated/model/product';
